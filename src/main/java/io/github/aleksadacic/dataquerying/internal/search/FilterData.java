559139a7613d7455e2260ff6e8e25853317dc80a
Aleksa Dacic <aleksa.dacic99@gmail.com>
2025-02-14 17:55:14 +0100

    Removed DataType completely.
    Signed-off-by: Aleksa Dacic <aleksa.dacic99@gmail.com>

--- src/main/java/io/github/aleksadacic/dataquerying/internal/search/FilterData.java
+++ src/main/java/io/github/aleksadacic/dataquerying/internal/search/FilterData.java
@@ -1,7 +1,6 @@
 package io.github.aleksadacic.dataquerying.internal.search;
 
 import io.github.aleksadacic.dataquerying.internal.enums.ConditionalOperator;
-import io.github.aleksadacic.dataquerying.internal.enums.DataType;
 import io.github.aleksadacic.dataquerying.api.SearchOperator;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +13,6 @@ public class FilterData {
     private String attribute;
     private Object value;
     private SearchOperator searchOperator; // EQ, NOT_EQ, GTE, LTE, IN, LIKE, etc.
-    private DataType type; // STRING, NUMBER, DATE, ARRAY, etc.
     private List<FilterData> filters; // nested filters
     private ConditionalOperator conditionalOperator; // AND, OR for a filter group
 }
